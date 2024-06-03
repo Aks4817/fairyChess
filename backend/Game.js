49f370800b940db010cc1e1239905760f06af6eb
Aks4817 <Anujkumarsingh4817@gmail.com>
2024-06-03 19:33:01 +0530

    multiplayer added

---
+++ backend/Game.js
@@ -0,0 +1,121 @@
+// const WebSocket = require("ws");
+const { GAME_OVER, INIT_GAME, MOVE } = require("./messages");
+
+class Game {
+  constructor(player1, player2,room,mode,state) {
+    this.player1 = player1;
+    this.room=room;
+    this.mode=mode;
+    this.state=state;
+    this.player2 = player2;
+    this.startTime = new Date();
+    this.moveCount = 0;
+
+    this.player1.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          room:room,
+          color: "white",
+          turns:0
+
+        },
+      })
+    );
+    this.player2.send(
+      JSON.stringify({
+        type: INIT_GAME,
+        payload: {
+          room:room,
+          color: "black",
+          turns:0
+        },
+      })
+    );
+    // this.player2.send(
+    //   JSON.stringify({
+    //     type: "updateData",
+    //     state: this.state,
+    //   })
+    // );
+
+    // this.player1.send(
+    //   JSON.stringify({
+    //     type: "updateData",
+    //     state: this.state,
+    //   })
+    // );
+
+  }
+  // changemode(socket ,selectMode){
+  //   this.mode=selectMode;
+  //   this.player1.send(JSON.stringify({
+  //     type:"selectedChange",
+  //     room:this.room,
+  //     selectedIndex:this.mode,
+  //   }))
+  //   this.player2.send(JSON.stringify({
+  //     type:"selectedChange",
+  //     room:this.room,
+  //     selectedIndex:this.mode,
+  //   }))
+  // }
+  makeMoves(socket, state) {
+    // Validate the type of move using zod
+
+    try {
+      this.moveCount++;
+      if(this.player2!==socket){
+
+        this.player2.send(
+          JSON.stringify({
+            type: "updateData",
+            state: state,
+          })
+        );
+        this.player2.send(
+            JSON.stringify({
+                type:"turnUpdate",
+
+            })
+        )   
+      }
+      if(this.player1!==socket){
+
+        this.player1.send(
+          JSON.stringify({
+            type: "updateData",
+            state: state,
+          })
+        );
+        this.player1.send(
+            JSON.stringify({
+                type:"turnUpdate",
+            })
+        )
+      }
+      
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+
+    // if (isGameOver()) {
+    //     // Send the game over message to both players
+    //     this.player1.emit(JSON.stringify({
+    //         type: GAME_OVER,
+    //         payload: {
+    //             winner: this.board.turn() === "w" ? "black" : "white"
+    //         }
+    //     }));
+    //     this.player2.emit(JSON.stringify({
+    //         type: GAME_OVER,
+    //         payload: {
+    //             winner: this.board.turn() === "w" ? "black" : "white"
+    //         }
+    //     }));
+    // }
+  }
+}
+
+module.exports = { Game };
---
+++ backend/GameManager.js
@@ -0,0 +1,56 @@
+const WebSocket = require("ws");
+const { INIT_GAME, MOVE } = require("./messages");
+const { Game } = require("./Game");
+
+class GameManager {
+    constructor() {
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
+    }
+
+    addUser(socket) {
+        this.users.push(socket);
+        this.addHandler(socket);
+    }
+
+    removeUser(socket) {
+        this.users = this.users.filter(user => user !== socket);
+        // Stop the game here because the user left
+    }
+
+    addHandler(socket) {
+        socket.on("message", (data) => {
+            const message = JSON.parse(data.toString());
+
+            if (message.type === INIT_GAME) {
+                if (this.pendingUser) {
+                    console.log("pendingUser");
+                    const game = new Game(this.pendingUser, socket ,message.room,message.selectedIndex,message.state);
+                    this.games.push(game);
+                    this.pendingUser = null;
+                } else {
+                    console.log("otherUser");
+                    this.pendingUser = socket;
+                }
+            }
+
+            if (message.type === "update") {
+                console.log("inside move");
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                if (game) {
+                    console.log("inside makemove");
+                    game.makeMoves(socket, message.state);
+                }
+            }
+            if(message.type==="selectedChange"){
+                const game = this.games.find(game => game.code===message.room || game.player1 === socket || game.player2 === socket);
+                game.changemode(socket, message.selectedIndex);
+            }
+
+
+        });
+    }
+}
+
+module.exports = { GameManager };
