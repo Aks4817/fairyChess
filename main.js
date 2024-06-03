6c567909b8583c156a51a4f660b4538f749918f3
Aks4817 <Anujkumarsingh4817@gmail.com>
2024-06-03 22:51:46 +0530

    different boards added to multiplayer

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// document.body.appendChild(c);
+
+document.body.appendChild(c);
 
 var scale = 2.0;
 
